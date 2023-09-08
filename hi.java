c00b13583ff5606eeb78dfe2802e7ab51e022567
santhosh <santhosh.kakileti@walmart.com>
2023-09-08 11:52:24 +0530

    hi1 change

--- hi.java
+++
@@ -1,5 +0,0 @@
-class Hello{
-    public static void main(string args []){
-        system.out.println("welcome to java via Jenkins build")
-    }
-}
---
+++ hi1.java
@@ -0,0 +1,5 @@
+class hi1{
+    public static void main(String[] args){
+        System.out.println("welcome to java via Jenkins build");
+    }
+}
